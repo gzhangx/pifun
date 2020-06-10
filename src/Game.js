8cf1791642dea1a6f8ab16f98263a834803046e8
Gang Zhang <gzhangx@hotmail.com>
2020-06-10 16:49:30 -0400

    'refactor'

--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,7 @@
 import React from "react";
-import p5 from "p5";
-import {createEngine} from "./components/world/engine";
+import {createEngine, setup} from "./components/platform/engine";
 
+import opt from './components/thisworld/world';
 const WIDTH = 600;
 const HEIGHT = 600;
 class Scene extends React.Component {
@@ -13,51 +13,9 @@ class Scene extends React.Component {
 
   
 
-  componentDidMount() {    
-      
-    const {addToWorld, Bodies} = createEngine();
-    const allBodies = [];
-
-    var ballA = Bodies.circle(210, 100, 30, { restitution: 0.5 });
-    var ballB = Bodies.circle(110, 50, 30, { restitution: 0.5 });
-    allBodies.push(ballA);
-    allBodies.push(ballB);
-    console.log(Bodies.rectangle(200, 0, 600, 50, { isStatic: true }));
-    addToWorld([
-      // walls      
-      Bodies.rectangle(WIDTH/2, HEIGHT, WIDTH, 60, { isStatic: true })
-    ])
-    
-
-    addToWorld([ballA, ballB]);
-    
-    
-
-    //Render.run((render));
-    const Sketch = p => {
-      p.setup = () => {
-        p.createCanvas(WIDTH, HEIGHT)
-      }
-  
-      p.draw = () => {
-        p.background(0);
-        p.fill(255);
-        allBodies.forEach(bdy=>{
-          p.rect(bdy.position.x - 30, bdy.position.y - 30, 60,60);
-        });
-        
-      }
-
-      p.mousePressed = ()=>{
-        console.log(p.mouseX)
-        const bdy = Bodies.circle(p.mouseX, p.mouseY, 30, { restitution: 0.7 });
-        allBodies.push(bdy);
-        console.log('x' +p.mouseX+' ' + p.mouseY)
-        addToWorld(bdy);
-        //World.add(engine.world, Bodies.circle(150, p.mouseX, p.mouseY, { restitution: 0.7 }));
-      }
-   }
-    this.myP5 = new p5(Sketch, this.drawRef.current);
+  componentDidMount() {          
+    const engine= createEngine();  
+    this.myP5 = setup(this.drawRef.current, opt, engine);
   }
 
   render() {
