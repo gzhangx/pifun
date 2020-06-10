9caa2d7301058bc7822fe33c41dcb50bbbb86858
Gang Zhang <gzhangx@hotmail.com>
2020-06-10 17:42:35 -0400

    'squre'

---
+++ src/components/objs/SimpleSqure.js
@@ -0,0 +1,24 @@
+export default function SimpleSqure(initInfo, engine) {
+    const {x,y,w, opts} = initInfo;
+    this.w = w;
+    this.opts = opts;
+    this.type = 'SimpleSqure';    
+    const {addToWorld, Bodies, allBodies} = engine;
+    const body = Bodies.rectangle(x, y, w, w, opts);
+    addToWorld(body);
+
+    allBodies.push(this);
+    this.show = p => {
+        const pos = body.position;
+        const angle = body.angle;
+        p.push();
+        p.translate(pos.x, pos.y);
+        p.rotate(angle);
+        p.rectMode(p.CENTER);
+        p.stroke(255);
+        p.strokeWeight(4);
+        p.fill(127);
+        p.rect(0,0, w);
+        p.pop();
+    }
+}
