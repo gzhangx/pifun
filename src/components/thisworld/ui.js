af82aff3f80832b2c0145d96813a3b5d6aacddf5
Gang Zhang <gzhangx@hotmail.com>
2020-07-01 00:08:57 -0400

    test

---
+++ src/components/thisworld/ui.js
@@ -0,0 +1,130 @@
+// Customized from https://raw.githubusercontent.com/liabru/matter-js/master/src/render/Render.js
+// Better than original p5js impl
+const Render = {
+    create: opt => {
+        return {
+            mouse: opt.mouse,
+            engine: opt.engine,
+            canvas: opt.canvas,
+            context: opt.canvas.getContext('2d'),
+            textures: {},
+        }
+    },
+};
+    
+function _applyBackground(render, background) {
+    let cssBackground = background;
+    if (/(jpg|gif|png)$/.test(background))
+        cssBackground = 'url(' + background + ')';
+
+    render.canvas.style.background = cssBackground;
+    render.canvas.style.backgroundSize = "contain";
+    render.currentBackground = background;
+}
+
+    Render.draw = function(core, render) {
+        const {createdEngine} = core;
+        const {Composite, engine, world, Bounds} = createdEngine;
+        const canvas = render.canvas,
+            context = render.context,
+            options = render.options,
+            allBodies = Composite.allBodies(world),
+            allConstraints = Composite.allConstraints(world),
+            background = options.background;
+        let bodies = [],
+            constraints = [],
+            i;
+
+        // apply background if it has changed
+        if (render.currentBackground !== background)
+            _applyBackground(render, background);
+
+        // clear the canvas with a transparent fill, to allow the canvas background to show
+        context.globalCompositeOperation = 'source-in';
+        context.fillStyle = "transparent";
+        context.fillRect(0, 0, canvas.width, canvas.height);
+        context.globalCompositeOperation = 'source-over';
+
+        // handle bounds
+        if (options.hasBounds) {
+            // filter out bodies that are not in view
+            for (i = 0; i < allBodies.length; i++) {
+                var body = allBodies[i];
+                if (Bounds.overlaps(body.bounds, render.bounds))
+                    bodies.push(body);
+            }
+
+            // filter out constraints that are not in view
+            for (i = 0; i < allConstraints.length; i++) {
+                var constraint = allConstraints[i],
+                    bodyA = constraint.bodyA,
+                    bodyB = constraint.bodyB,
+                    pointAWorld = constraint.pointA,
+                    pointBWorld = constraint.pointB;
+
+                if (bodyA) pointAWorld = Vector.add(bodyA.position, constraint.pointA);
+                if (bodyB) pointBWorld = Vector.add(bodyB.position, constraint.pointB);
+
+                if (!pointAWorld || !pointBWorld)
+                    continue;
+
+                if (Bounds.contains(render.bounds, pointAWorld) || Bounds.contains(render.bounds, pointBWorld))
+                    constraints.push(constraint);
+            }
+
+            // transform the view
+            Render.startViewTransform(render);
+
+            // update mouse
+            if (render.mouse) {
+                Mouse.setScale(render.mouse, {
+                    x: (render.bounds.max.x - render.bounds.min.x) / render.options.width,
+                    y: (render.bounds.max.y - render.bounds.min.y) / render.options.height
+                });
+
+                Mouse.setOffset(render.mouse, render.bounds.min);
+            }
+        } else {
+            constraints = allConstraints;
+            bodies = allBodies;
+
+            if (render.options.pixelRatio !== 1) {
+                render.context.setTransform(render.options.pixelRatio, 0, 0, render.options.pixelRatio, 0, 0);
+            }
+        }
+
+        // fully featured rendering of bodies
+        Render.bodies(render, bodies, context);
+
+        if (options.showBounds)
+            Render.bodyBounds(render, bodies, context);
+
+        if (options.showAxes || options.showAngleIndicator)
+            Render.bodyAxes(render, bodies, context);
+
+        if (options.showPositions)
+            Render.bodyPositions(render, bodies, context);
+
+        if (options.showVelocity)
+            Render.bodyVelocity(render, bodies, context);
+
+        if (options.showIds)
+            Render.bodyIds(render, bodies, context);
+
+        if (options.showSeparations)
+            Render.separations(render, engine.pairs.list, context);
+
+        if (options.showCollisions)
+            Render.collisions(render, engine.pairs.list, context);
+
+        if (options.showMousePosition)
+            Render.mousePosition(render, render.mouse, context);
+
+        Render.constraints(constraints, context);
+
+        if (options.hasBounds) {
+            // revert view transforms
+            Render.endViewTransform(render);
+        }
+
+    };
