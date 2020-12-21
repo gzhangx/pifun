d37cb019242c2c5d8b9028163e821cba73898dc0
Gang Zhang <gzhangx@hotmail.com>
2020-12-21 17:24:10 -0500

    adding socket

---
+++ src/components/thisworld/socket.js
@@ -0,0 +1,33 @@
+const urlBase = 'http://192.168.1.41:8081';
+const WsSocket = {
+    socket: null,
+    freeFormMsgListener: null,
+}
+
+export function setListener(lsn) {
+    WsSocket.freeFormMsgListener = lsn;
+}
+export function sendWsMsg(msg) {
+    if (!WsSocket.socket) return;
+    WsSocket.socket.emit('ggFreeFormMsg', msg);
+}
+export function initWS() {
+    if (!WsSocket.socket) {
+        const socket = require('socket.io-client')(urlBase, {
+            transports: ['websocket'],
+            path: '/pmapi/socket.io'
+        });
+        WsSocket.socket = socket;
+        socket.on('connect', function () {
+            console.log('connect')
+        });        
+        socket.on('ggFreeFormMsg', msg => {
+            if (WsSocket.freeFormMsgListener) {
+                WsSocket.freeFormMsgListener(msg);
+            }
+        })
+        socket.on('disconnect', function () {
+            console.log('disconnet')
+        });
+    }
+}
